81a46fee5b216ccfcb27e60d7c3e2f190984b85b
Josh Silverman <joshs.silverman@gmail.com>
2013-11-27 10:10:51 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WisrFeed::Application.config.secret_key_base = 'ddfbfa7ef26930951b64b9db28e27a9a98538cf9d0dc889ed24c85212a06a21cedaf2307b8d8260b7a359a4b9726b8b3e7e558cfb8cb19d3131cfcae45c5f2ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WisrFeed::Application.config.session_store :cookie_store, key: '_wisr-feed_session'
