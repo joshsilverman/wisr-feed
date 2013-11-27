f1b63dea955d7c82eeb4e5ead005d920c804a275
Josh Silverman <joshs.silverman@gmail.com>
2013-11-27 12:23:34 -0500

    61554178 shared tokens and pry

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-WisrFeed::Application.config.session_store :cookie_store, key: '_wisr-feed_session'
+# WisrFeed::Application.config.session_store :cookie_store, key: '_wisr-feed_session'
+WisrFeed::Application.config.session_store :cookie_store, key: '_quizmemanager_session'
+
+# Quizmemanager::Application.config.session_store :cookie_store, key: '_quizmemanager_session', domain: :all
